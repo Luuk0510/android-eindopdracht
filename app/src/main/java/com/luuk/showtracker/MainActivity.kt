c75793005a00be9bc2860eed4d8d1e56c1fff51f
Luuk <0510luuk@gmail.com>
2026-03-18 15:41:37 +0100

    Add Secrets plugin and default secrets file
    Integrate the Google Secrets Gradle plugin to manage API keys and other secrets. App module configured to use secrets.properties with a checked-in local.defaults.properties template (MY_API_KEY=template_key_build_will_not_fail) so builds don't break; secrets.properties is added to .gitignore to keep real secrets out of VCS. Updated libs.versions.toml with the plugin version and added the plugin alias to both app and root build files. Minor MainActivity UI change: wrap content in a Column, add padding, and a placeholder comment showing where to use the API key.

--- app/src/main/java/com/luuk/showtracker/MainActivity.kt
+++ app/src/main/java/com/luuk/showtracker/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.luuk.showtracker.ui.theme.ShowTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +22,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShowTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
+                        Greeting(name = "Android")
+
+                        // Use your API key here:
+                    }
                 }
             }
         }
