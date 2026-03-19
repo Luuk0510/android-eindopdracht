2c26a6c09ca0634fd9d6ee84ab71769575764b55
Luuk <0510luuk@gmail.com>
2026-03-19 13:54:15 +0100

    add api list

---
+++ app/src/main/java/com/luuk/showtracker/data/api/TmdbService.kt
@@ -0,0 +1,12 @@
+package com.luuk.showtracker.data.api
+
+import com.luuk.showtracker.data.model.TmdbResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TmdbService {
+    @GET("trending/all/day")
+    suspend fun getTrending(
+        @Query("api_key") apiKey: String
+    ): TmdbResponse
+}
